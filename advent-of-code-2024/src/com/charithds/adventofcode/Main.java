e24762f00aee7b945bdc5d0dfe826fb5eb4ab819
Soysa <charithssc@gmail.com>
2025-07-03 01:48:26 +0800

    5th problem completed

--- advent-of-code-2024/src/com/charithds/adventofcode/Main.java
+++ advent-of-code-2024/src/com/charithds/adventofcode/Main.java
@@ -3,6 +3,8 @@ package com.charithds.adventofcode;
 import com.charithds.adventofcode.day2.Day2;
 import com.charithds.adventofcode.day3.Day3;
 import com.charithds.adventofcode.day4.Day4;
+import com.charithds.adventofcode.day5.Day5;
+import com.charithds.adventofcode.day5.Day5Part2;
 
 class Main {
     public static void main(String[] args) {
@@ -13,7 +15,13 @@ class Main {
 //        Day3 day3 = new Day3();
 //        day3.execute();
 
-        Day4 day4 = new Day4();
-        day4.execute();
+//        Day4 day4 = new Day4();
+//        day4.execute();
+
+//        Day5 day5 = new Day5();
+//        day5.execute();
+
+        Day5Part2 day5Part2 = new Day5Part2();
+        day5Part2.execute();
     }
 }
