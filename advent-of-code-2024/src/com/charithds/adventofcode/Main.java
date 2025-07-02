f2742a08d4b46fa95941d2ee3b92582b3789b00c
Soysa <charithssc@gmail.com>
2025-07-02 17:11:39 +0800

    task 2 completed

---
+++ advent-of-code-2024/src/com/charithds/adventofcode/Main.java
@@ -0,0 +1,11 @@
+package com.charithds.adventofcode;
+
+import com.charithds.adventofcode.day2.Day2;
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, Advent of Code!");
+        Day2 day2  = new Day2();
+        day2.execute();
+    }
+}
---
+++ advent-of-code-2024/src/com/charithds/adventofcode/day2/Day2.java
@@ -0,0 +1,57 @@
+package com.charithds.adventofcode.day2;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Day2 {
+    private static final String DAY2_FILE = "resources/day2.dat";
+
+    public List<List<Integer>> getInput() {
+        try {
+            return Files.lines(Paths.get(DAY2_FILE))
+                    .map(line -> Arrays.stream(line.split(" "))
+                            .map(Integer::parseInt)
+                            .collect(Collectors.toList())
+                    ).toList();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public boolean isSafe(List<Integer> list) {
+        int direction = list.get(1) - list.get(0) > 0 ? 1 : -1;
+        for (int i = 0; i < list.size() - 1; i++) {
+            var diff = list.get(i + 1) - list.get(i);
+            var diffWithDirection = diff * direction;
+            if (diffWithDirection < 1 || diffWithDirection > 3) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void execute() {
+        List<List<Integer>> input = getInput();
+        if (input == null) {
+            System.out.println("Failed to read input data.");
+            return;
+        }
+
+        int safeCount = 0;
+        for (List<Integer> list : input) {
+            if (isSafe(list)) {
+                safeCount++;
+            }
+        }
+
+        System.out.println("Number of safe lists: " + safeCount);
+    }
+
+}
