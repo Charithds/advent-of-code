c177bfc633c50ea040ce8ab73ecf3e0098720723
Soysa <charithssc@gmail.com>
2025-07-03 16:24:45 +0800

    fixing gitignore again

--- advent-of-code-2024/src/com/charithds/adventofcode/Main.java
+++ advent-of-code-2024/src/com/charithds/adventofcode/Main.java
@@ -1,11 +1,5 @@
 package com.charithds.adventofcode;
 
-import com.charithds.adventofcode.day2.Day2;
-import com.charithds.adventofcode.day3.Day3;
-import com.charithds.adventofcode.day4.Day4;
-import com.charithds.adventofcode.day5.Day5;
-import com.charithds.adventofcode.day5.Day5Part2;
-
 class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Advent of Code!");
@@ -21,7 +15,7 @@ class Main {
 //        Day5 day5 = new Day5();
 //        day5.execute();
 
-        Day5Part2 day5Part2 = new Day5Part2();
-        day5Part2.execute();
+//        Day5Part2 day5Part2 = new Day5Part2();
+//        day5Part2.execute();
     }
 }
